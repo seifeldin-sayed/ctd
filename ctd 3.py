7f48e6abdc04c05ef7818bd5ba91b758a57660a3
seifeldin-sayed <96207423+seifeldin-sayed@users.noreply.github.com>
2022-02-25 18:35:59 +0200

    Update ctd 3.py

--- ctd 3.py
+++ ctd 3.py
@@ -1,18 +1,19 @@
-from ast import In
-bag=[6,22,10,999,76,43]
 def enter_number(bag):
     number=input("Enter a number: ")
     if number.isdigit():
-        bag.append(number)
+        bag.append(int(number))
     else:
         return(enter_number(bag))
 def remove_number(bag):
     number=input("Enter a number: ")
     if number.isdigit():
-        if number in bag:
-            bag.remove(number)
+        if int(number) in bag:
+            bag.remove(int(number))
+        else:
+            print(number + " not in list" )
     else:
         return(remove_number(bag))
+bag=[6,22,10,999,76,43]
 while True:
     print(bag)
     Type=input("choose enter or remove or sort or stop: ").lower()
