42892a6ac4b049dad730c6f5a6994e7c9bf7662b
Seifeldin Sayed <seifeldin18102002@gmail.com>
2023-08-20 19:52:26 +0300

    calculator

---
+++ calculator.py
@@ -0,0 +1,249 @@
+print("PLEASE ENTER YOUR GRADE FROM 0 TO 100 IN EACH SUBJECT")
+a=float(input("physics grade:"))
+while a>100 or a<0:
+    print("ERROR!")
+    a=float(input("physics grade:"))
+if a>=97:
+    a=12
+    print("YOUR GRADE IS:","A+")
+elif a>=93:
+    a=11.5
+    print("YOUR GRADE IS:","A")
+elif a>=89:
+    a=11
+    print("YOUR GRADE IS:","A-")
+elif a>=84:
+    a=10
+    print("YOUR GRADE IS:","B+")
+elif a>=80:
+    a=9
+    print("YOUR GRADE IS:","B")
+elif a>=76:
+    a=8
+    print("YOUR GRADE IS:","B-")
+elif a>=73:
+    a=7
+    print("YOUR GRADE IS:","C+")
+elif a>=70:
+    a=6
+    print("YOUR GRADE IS:","C")
+elif a>=67:
+    a=5
+    print("YOUR GRADE IS:","C-")
+elif a>=64:
+    a=4
+    print("YOUR GRADE IS:","D+")
+elif a>=60:
+    a=3
+    print("YOUR GRADE IS:","D")
+else:
+    a=0
+    print("YOUR GRADE IS:","faild")
+b=float(input("engineering mechanics grade:"))
+while b>100 or b<0:
+    print("ERROR!")
+    b=float(input("engineering mechanics grade:"))
+if b>=97:
+    b=12
+    print("YOUR GRADE IS:","A+")
+elif b>=93:
+    b=11.5
+    print("YOUR GRADE IS:","A")
+elif b>=89:
+    b=11
+    print("YOUR GRADE IS:","A-")
+elif b>=84:
+    b=10
+    print("YOUR GRADE IS:","B+")
+elif b>=80:
+    b=9
+    print("YOUR GRADE IS:","B")
+elif b>=76:
+    b=8
+    print("YOUR GRADE IS:","B-")
+elif b>=73:
+    b=7
+    print("YOUR GRADE IS:","C+")
+elif b>=70:
+    b=6
+    print("YOUR GRADE IS:","C")
+elif b>=67:
+    b=5
+    print("YOUR GRADE IS:","C-")
+elif b>=64:
+    b=4
+    print("YOUR GRADE IS:","D+")
+elif b>=60:
+    b=3
+    print("YOUR GRADE IS:","D")
+elif b<0:
+    b=0
+    print("YOUR GRADE IS:","faild")
+else:
+    b=0
+    print("YOUR GRADE IS:","faild")
+c=float(input("mathematics grade:"))
+while c>100 or c<0:
+    print("ERROR!")
+    c=float(input("mathematics grade:"))
+if c>=97:
+    c=12
+    print("YOUR GRADE IS:","A+")
+elif c>=93:
+    c=11.5
+    print("YOUR GRADE IS:","A")
+elif c>=89:
+    c=11
+    print("YOUR GRADE IS:","A-")
+elif c>=84:
+    c=10
+    print("YOUR GRADE IS:","B+")
+elif c>=80:
+    c=9
+    print("YOUR GRADE IS:","B")
+elif c>=76:
+    c=8
+    print("YOUR GRADE IS:","B-")
+elif c>=73:
+    c=7
+    print("YOUR GRADE IS:","C+")
+elif c>=70:
+    c=6
+    print("YOUR GRADE IS:","C")
+elif c>=67:
+    c=5
+    print("YOUR GRADE IS:","C-")
+elif c>=64:
+    c=4
+    print("YOUR GRADE IS:","D+")
+elif c>=60:
+    c=3
+    print("YOUR GRADE IS:","D")
+else:
+    c=0
+    print("YOUR GRADE IS:","faild")
+d=float(input("intro. to computer grade:"))
+while d>100 or d<0:
+    print("ERROR!")
+    d=float(input("intro. to computer grade:"))
+if d>=97:
+    d=12
+    print("YOUR GRADE IS:","A+")
+elif d>=93:
+    d=11.5
+    print("YOUR GRADE IS:","A")
+elif d>=89:
+    d=11
+    print("YOUR GRADE IS:","A-")
+elif d>=84:
+    d=10
+    print("YOUR GRADE IS:","B+")
+elif d>=80:
+    d=9
+    print("YOUR GRADE IS:","B")
+elif d>=76:
+    d=8
+    print("YOUR GRADE IS:","B-")
+elif d>=73:
+    d=7
+    print("YOUR GRADE IS:","C+")
+elif d>=70:
+    d=6
+    print("YOUR GRADE IS:","C")
+elif d>=67:
+    d=5
+    print("YOUR GRADE IS:","C-")
+elif d>=64:
+    d=4
+    print("YOUR GRADE IS:","D+")
+elif d>=60:
+    d=3
+    print("YOUR GRADE IS:","D")
+else:
+    d=0
+    print("YOUR GRADE IS:","faild")
+e=float(input("chemistry grade:"))
+while e>100 or e<0:
+    print("ERROR!")
+    e=float(input("chemistry grade:"))
+if e>=97:
+    e=8
+    print("YOUR GRADE IS:","A+")
+elif e>=93:
+    e=7.666666667
+    print("YOUR GRADE IS:","A")
+elif e>=89:
+    e=7.333333333
+    print("YOUR GRADE IS:","A-")
+elif e>=84:
+    e=6.666666667
+    print("YOUR GRADE IS:","B+")
+elif e>=80:
+    e=6
+    print("YOUR GRADE IS:","B")
+elif e>=76:
+    e=5.333333333
+    print("YOUR GRADE IS:","B-")
+elif e>=73:
+    e=4.666666667
+    print("YOUR GRADE IS:","C+")
+elif e>=70:
+    e=4
+    print("YOUR GRADE IS:","C")
+elif e>=67:
+    e=3.333333333
+    print("YOUR GRADE IS:","C-")
+elif e>=64:
+    e=2.666666667
+    print("YOUR GRADE IS:","D+")
+elif e>=60:
+    e=2
+    print("YOUR GRADE IS:","D")
+else:
+    e=0
+    print("YOUR GRADE IS:","faild")
+f=float(input("industrial relations grade:"))
+while f>100 or f<0:
+    print("ERROR!")
+    f=float(input("industrial relations grade:"))
+if f>=97:
+    f=8
+    print("YOUR GRADE IS:","A+")
+elif f>=93:
+    f=7.6666666667
+    print("YOUR GRADE IS:","A")
+elif f>=89:
+    f=7.333333333
+    print("YOUR GRADE IS:","A-")
+elif f>=84:
+    f=6.666666667
+    print("YOUR GRADE IS:","B+")
+elif f>=80:
+    f=6
+    print("YOUR GRADE IS:","B")
+elif f>=76:
+    f=5.333333333
+    print("YOUR GRADE IS:","B-")
+elif f>=73:
+    f=4.666666667
+    print("YOUR GRADE IS:","C+")
+elif f>=70:
+    f=4
+    print("YOUR GRADE IS:","C")
+elif f>=67:
+    f=3.333333333
+    print("YOUR GRADE IS:","C-")
+elif f>=64:
+    f=2.666666667
+    print("YOUR GRADE IS:","D+")
+elif f>=60:
+    f=2
+    print("YOUR GRADE IS:","D")
+else:
+    f=0
+    print("YOUR GRADE IS:","faild")
+t=((a+b+c+d+e+f)/16)
+if t>=2:
+    print("YOUR GPA IS:",t)
+else:
+    print("YOUR GPA IS:","FAILD")
